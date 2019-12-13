12f4df8480f2a1a450e762260c4b3319e1838388
Ivan <mikheev.ivan91@gmail.com>
2019-12-13 19:29:54 +0300

    add DAO pattern

---
+++ src/main/java/com/notfound/dao/FilmDAO.java
@@ -0,0 +1,18 @@
+package com.notfound.dao;
+
+import com.notfound.model.Film;
+
+import java.util.List;
+
+public interface FilmDAO {
+
+    List<Film> allFilms();
+
+    void add(Film film);
+
+    void delete(Film film);
+
+    void edit(Film film);
+
+    Film getById(int id);
+}
