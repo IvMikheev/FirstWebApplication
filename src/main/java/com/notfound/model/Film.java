8f3ad98d13acbeedc05733b13fed0554a90a52fb
Ivan <mikheev.ivan91@gmail.com>
2019-12-14 19:21:26 +0300

    add DB support

--- src/main/java/com/notfound/model/Film.java
+++ src/main/java/com/notfound/model/Film.java
@@ -1,10 +1,26 @@
 package com.notfound.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "films")
 public class Film {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "year")
     private int year;
+
+    @Column(name = "genre")
     private String genre;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "watched")
     private boolean watched;
 
     public int getId() {
