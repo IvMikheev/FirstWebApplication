fe54bb64d3fa4ea654367619cd8c17ff85d10d5c
Ivan <mikheev.ivan91@gmail.com>
2019-12-14 19:25:13 +0300

    add DB support

--- src/main/java/com/notfound/service/FilmServiceImpl.java
+++ src/main/java/com/notfound/service/FilmServiceImpl.java
@@ -4,6 +4,7 @@ import com.notfound.dao.FilmDAO;
 import com.notfound.model.Film;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -17,26 +18,31 @@ public class FilmServiceImpl implements FilmService {
     }
 
     @Override
+    @Transactional
     public List<Film> allFilms() {
         return filmDAO.allFilms();
     }
 
     @Override
+    @Transactional
     public void add(Film film) {
         filmDAO.add(film);
     }
 
     @Override
+    @Transactional
     public void delete(Film film) {
         filmDAO.delete(film);
     }
 
     @Override
+    @Transactional
     public void edit(Film film) {
         filmDAO.edit(film);
     }
 
     @Override
+    @Transactional
     public Film getById(int id) {
         return filmDAO.getById(id);
     }
